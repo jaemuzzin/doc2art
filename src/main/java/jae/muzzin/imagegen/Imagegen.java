ebe6a103a7963c15c22979cb9ef630e7eade4168
Jae Muzzin <jae@sonar.software>
2023-03-21 23:06:05 -0400

    way less gen cycles

--- src/main/java/jae/muzzin/imagegen/Imagegen.java
+++ src/main/java/jae/muzzin/imagegen/Imagegen.java
@@ -127,12 +127,12 @@ public class Imagegen {
             sd.convertToConstants(Arrays.asList(new SDVariable[]{sd.getVariable("disc_w0"), sd.getVariable("disc_w1"), sd.getVariable("disc_b0"), sd.getVariable("disc_b1")}));
 
             System.err.println("Training GEN...");
-            for (int e = 0; e < 1000; e++) {
+            for (int e = 0; e < 50; e++) {
                 DataSet gends = new DataSet(Nd4j.rand(DataType.FLOAT, batchSize, 10), fakeGenTrainingLables);
                 sd.fit(gends);
-                if (e % 50 == 0) {
+                //if (e % 5 == 0) {
                     sd.evaluate(new ViewIterator(gends, Math.min(batchSize, gends.numExamples() - 1)), "disc", evaluation);
-                }
+                //}
             }
             System.err.println(evaluation.confusionMatrix());
             //print gen example
