f5d5f8fb5647d1e44b6441538518f8ffc487f3d3
Jae Muzzin <jae@sonar.software>
2023-03-23 01:31:15 -0400

    see

--- src/main/java/jae/muzzin/imagegen/Imagegen.java
+++ src/main/java/jae/muzzin/imagegen/Imagegen.java
@@ -168,14 +168,14 @@ public class Imagegen {
                 sd.evaluate(new ViewIterator(gends, Math.min(batchSize, gends.numExamples() - 1)), "disc", evaluation);
                 if(e % 10==0){
                     sd.getVariable("generator_input").setArray(Nd4j.rand(DataType.FLOAT, 1, 8, 5, 5));
-                    var imageOutput = sd.math.step(generator, 0.3).eval().reshape(1, 28, 28);
+                    var imageOutput = sd.math.step(generator, 0.5).eval().reshape(1, 28, 28);
                     System.err.println(imageOutput.toStringFull().replaceAll(" ", "").replaceAll("1", "*").replaceAll("0", " ").replaceAll(",", ""));
                 }
             }
             System.err.println(evaluation.confusionMatrix());
             //print gen example
             sd.getVariable("generator_input").setArray(Nd4j.rand(DataType.FLOAT, 1, 8, 5, 5));
-            var imageOutput = sd.math.step(generator, 0.3).eval().reshape(1, 28, 28);
+            var imageOutput = sd.math.step(generator, 0.5).eval().reshape(1, 28, 28);
             System.err.println(imageOutput.toStringFull().replaceAll(" ", "").replaceAll("1", "*").replaceAll("0", " ").replaceAll(",", ""));
 
             sd.save(new File("gan.model"), true);
